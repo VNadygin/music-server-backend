46b7098d869b7d6cf9c85583ee684f3fc8d4ac5a
Vladimir Nadygin <vnadygin@me.com>
2019-02-01 19:41:18 +0300

    minor change

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(router);
 app.use(cors());
 
-app.listen(config.server.port, config.server.host, () => {
+app.listen(config.server.port, () => {
   // tslint:disable-next-line:no-console
   console.log(`Listening ${config.server.host}:${config.server.port}`);
 });
